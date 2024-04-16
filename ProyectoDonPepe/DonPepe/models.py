593546adb545704fe98b711752a0f82513b03f92
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2024-04-15 21:01:38 -0400

    Update models.py

--- ProyectoDonPepe/DonPepe/models.py
+++ ProyectoDonPepe/DonPepe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+
+    rut                 = models.CharField(primary_key=True,max_length=10)
+    nombre              = models.CharField(max_length=20)
+    apellido            = models.CharField(max_length=20)
+    fecha_nacimiento    = models.DateField(blank=False,null=False)
+    telefono            = models.CharField(max_length=45)
+    correo               = models.EmailField(unique=True,max_length=100, blank=True, null=True)
+    clave          = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.nombre  
