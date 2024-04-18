739ec8f55c108f39a1becc6b57b668b534ef2066
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2024-04-18 15:21:33 -0400

    Registro casi terminado

--- ProyectoDonPepe/core/apps.py
+++ ProyectoDonPepe/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DonpepeConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DonPepe'
+    name = 'core'
