6df8979ac743d78dc1eccf59b7aca91b483c6fe4
ElDotto <ignaibarra21@gmail.com>
2024-04-23 01:32:21 -0400

    Cambios a DJANGO
    Se crearon los respectivos cambios y botones a django, se comenzo a utilizar BOOTSTRAP 4, se integraron fotografias y secciones de categorias en los productos

--- ProyectoDonPepe/core/urls.py
+++ ProyectoDonPepe/core/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('register/', views.register, name="register"),
+    path('productos/', views.productos, name="productos"),
+    path('quienessomos/', views.quienessomos, name="quienessomos"),
+    path('galeria/', views.galeria, name="galeria"),
 ]
--- ProyectoDonPepe/core/views.py
+++ ProyectoDonPepe/core/views.py
@@ -7,4 +7,11 @@ def inicio(request):
 def register(request):
     return render(request, 'core/register.html')
 
+def productos(request):
+    return render(request, 'core/productos.html')
 
+def quienessomos(request):
+    return render(request, 'core/quienessomos.html')
+
+def galeria(request):
+    return render(request, 'core/galeria.html')
